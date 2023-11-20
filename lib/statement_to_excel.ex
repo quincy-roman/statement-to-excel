fdf519974d9f8bb34791f7e1a6e55dbef25aa396
metsuryuu <qjroman@gmail.com>
2023-11-20 18:24:51 -0500

    Init

---
+++ lib/statement_to_excel.ex
@@ -0,0 +1,18 @@
+defmodule StatementToExcel do
+  @moduledoc """
+  Documentation for `StatementToExcel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StatementToExcel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/statement_to_excel_test.exs
@@ -0,0 +1,8 @@
+defmodule StatementToExcelTest do
+  use ExUnit.Case
+  doctest StatementToExcel
+
+  test "greets the world" do
+    assert StatementToExcel.hello() == :world
+  end
+end
